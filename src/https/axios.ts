db9c6cb02932edbeb958bf8c18e9df5b90a4b52d
Shavkat838 <boltayevshavkat792@gmail.com>
2026-06-04 16:54:52 +0500

    api url changed

--- src/https/axios.ts
+++ src/https/axios.ts
@@ -1,13 +1,6 @@
 import axios from "axios"
 
-const API_URL="http://localhost:8080/api/notes"
-export const $api=axios.create({
-    withCredentials:true,
-    baseURL:API_URL,
-})
-
-
-const API_AUTH_URL="http://localhost:8080/api/users"
+const API_AUTH_URL=`${import.meta.env.VITE_API_URL}/users`
 export const $authApi=axios.create({
     withCredentials:true,
     baseURL:API_AUTH_URL
