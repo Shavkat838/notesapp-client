22346ddeb8bf3e6a0a39b76d787deb947a264d4e
Shavkat838 <boltayevshavkat792@gmail.com>
2026-06-04 16:52:59 +0500

    api url changed

--- src/https/api.ts
+++ src/https/api.ts
@@ -3,14 +3,12 @@ import axios from "axios";
 
 
 
-const APi_POST_URL="http://localhost:8080/api/notes"
+const APi_POST_URL=`${import.meta.env.VITE_API_URL}/notes`
 
 const $api=axios.create({
     withCredentials:true,
     baseURL:APi_POST_URL
 })
-
-
 $api.interceptors.request.use(config =>{
     config.headers.Authorization=`Bearer ${localStorage.getItem('accessToken')}`
     return config
